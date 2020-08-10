7d6ca706aa69ebc381d30f3064f1771c5745f732
Keirron Stach <stachkeirron@gmail.com>
2020-08-10 20:40:25 +0930

    Dockerfile copy

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,21 +5,21 @@ echo "Starting Discord deploy"
 ls
 
 mkdir ~/.dispatch
-cp /Dispatch/credentials.json ~/.dispatch/credentials.json
+cp $RUNNER_SPACE/Dispatch/credentials.json ~/.dispatch/credentials.json
 sed -i "s/app_id_goes_here/$INPUT_APPID/" ~/.dispatch/credentials.json
 sed -i "s/token_goes_here/$INPUT_BOTTOKEN/" ~/.dispatch/credentials.json
-chmod +x /Dispatch/dispatch
+chmod +x $RUNNER_SPACE/Dispatch/dispatch
 
-/Dispatch/dispatch branch list $INPUT_APPID > branches.txt
+$RUNNER_SPACE/Dispatch/dispatch branch list $INPUT_APPID > branches.txt
 if grep -q $INPUT_BRANCHID branches.txt; then
   echo "branch exists"
 else
   echo "branch does not exists; creating"
-  /Dispatch/dispatch branch create $INPUT_APPID $INPUT_BRANCHID
+  $RUNNER_SPACE/Dispatch/dispatch branch create $INPUT_APPID $INPUT_BRANCHID
 fi
 
-/Dispatch/dispatch branch list $INPUT_APPID > branches.txt
+$RUNNER_SPACE/Dispatch/dispatch branch list $INPUT_APPID > branches.txt
 BRANCH_ID=$(grep $INPUT_BRANCHID branches.txt | cut -d'|' -f3 - | tr -d '[:space:]')
-/Dispatch/dispatch build push $BRANCH_ID $GITHUB_WORKSPACE/$INPUT_CONFIGPATH $GITHUB_WORKSPACE/$INPUT_BUILDPATH -p
+$RUNNER_SPACE/Dispatch/dispatch build push $BRANCH_ID $GITHUB_WORKSPACE/$INPUT_CONFIGPATH $GITHUB_WORKSPACE/$INPUT_BUILDPATH -p
 
 echo "Discord deploy completed"
