ec8b4aa8116104c4766a8af35d360e9c18d07674
Keirron Stach <stachkeirron@gmail.com>
2020-08-10 21:15:44 +0930

    Copy items into github workspace

--- Dockerfile
+++ Dockerfile
@@ -1,8 +1,8 @@
 # Container image that runs your code
 FROM alpine:3.10
 
-ADD Dispatch /Dispatch
-RUN chmod -R +x /Dispatch
+ADD Dispatch $GITHUB_WORKSPACE/Dispatch
+RUN chmod -R +x $GITHUB_WORKSPACE/Dispatch
 ADD entrypoint.sh /entrypoint.sh
 
 # Code file to execute when the docker container starts up (`entrypoint.sh`)
--- entrypoint.sh
+++ entrypoint.sh
@@ -5,21 +5,21 @@ echo "Starting Discord deploy"
 ls
 
 mkdir ~/.dispatch
-cp $RUNNER_SPACE/Dispatch/credentials.json ~/.dispatch/credentials.json
+cp /Dispatch/credentials.json ~/.dispatch/credentials.json
 sed -i "s/app_id_goes_here/$INPUT_APPID/" ~/.dispatch/credentials.json
 sed -i "s/token_goes_here/$INPUT_BOTTOKEN/" ~/.dispatch/credentials.json
-chmod +x $RUNNER_SPACE/Dispatch/dispatch
+chmod +x /Dispatch/dispatch
 
-$RUNNER_SPACE/Dispatch/dispatch branch list $INPUT_APPID > branches.txt
+/Dispatch/dispatch branch list $INPUT_APPID > branches.txt
 if grep -q $INPUT_BRANCHID branches.txt; then
   echo "branch exists"
 else
   echo "branch does not exists; creating"
-  $RUNNER_SPACE/Dispatch/dispatch branch create $INPUT_APPID $INPUT_BRANCHID
+  /Dispatch/dispatch branch create $INPUT_APPID $INPUT_BRANCHID
 fi
 
-$RUNNER_SPACE/Dispatch/dispatch branch list $INPUT_APPID > branches.txt
+/Dispatch/dispatch branch list $INPUT_APPID > branches.txt
 BRANCH_ID=$(grep $INPUT_BRANCHID branches.txt | cut -d'|' -f3 - | tr -d '[:space:]')
-$RUNNER_SPACE/Dispatch/dispatch build push $BRANCH_ID $GITHUB_WORKSPACE/$INPUT_CONFIGPATH $GITHUB_WORKSPACE/$INPUT_BUILDPATH -p
+/Dispatch/dispatch build push $BRANCH_ID $GITHUB_WORKSPACE/$INPUT_CONFIGPATH $GITHUB_WORKSPACE/$INPUT_BUILDPATH -p
 
 echo "Discord deploy completed"
