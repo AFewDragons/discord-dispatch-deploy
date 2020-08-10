d435bf6259e0d65b631c8b683ce38e2752db39f9
Keirron Stach <stachkeirron@gmail.com>
2020-08-10 13:33:30 +0930

    Update to use proper dispatch folders

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,30 +2,22 @@
 
 echo "Starting Dispatch deploy build push"
 
-echo "$GITHUB_ACTION_PATH"
-echo "$GITHUB_EVENT_PATH"
-echo "$GITHUB_ACTION"
-echo "$GITHUB_WORKSPACE"
-
-cd $GITHUB_WORKSPACE
-ls
-
 mkdir ~/.dispatch
-cp $GITHUB_ACTION_PATH/Dispatch/credentials.json ~/.dispatch/credentials.json
+cp /Dispatch/credentials.json ~/.dispatch/credentials.json
 sed -i "s/app_id_goes_here/$INPUT_APPID/" ~/.dispatch/credentials.json
 sed -i "s/token_goes_here/$INPUT_BOTTOKEN/" ~/.dispatch/credentials.json
-chmod +x $GITHUB_ACTION_PATH/Dispatch/dispatch
+chmod +x /Dispatch/dispatch
 
-$GITHUB_ACTION_PATH/Dispatch/dispatch branch list $INPUT_APPID > branches.txt
+/Dispatch/dispatch branch list $INPUT_APPID > branches.txt
 if grep -q $INPUT_BRANCHID branches.txt; then
   echo "branch exists"
 else
   echo "branch does not exists; creating"
-  $GITHUB_ACTION_PATH/Dispatch/dispatch branch create $INPUT_APPID $INPUT_BRANCHID
+  /Dispatch/dispatch branch create $INPUT_APPID $INPUT_BRANCHID
 fi
 
-$GITHUB_ACTION_PATH/Dispatch/dispatch branch list $INPUT_APPID > branches.txt
+/Dispatch/dispatch branch list $INPUT_APPID > branches.txt
 BRANCH_ID=$(grep $INPUT_BRANCHID branches.txt | cut -d'|' -f3 - | tr -d '[:space:]')
-$GITHUB_ACTION_PATH/Dispatch/dispatch build push $BRANCH_ID $GITHUB_WORKSPACE/$INPUT_CONFIGPATH $GITHUB_WORKSPACE/$INPUT_BUILDPATH -p
+/Dispatch/dispatch build push $BRANCH_ID $GITHUB_WORKSPACE/$INPUT_CONFIGPATH $GITHUB_WORKSPACE/$INPUT_BUILDPATH -p
 
 echo "Dispatch deploy for application $INPUT_APPID completed"
